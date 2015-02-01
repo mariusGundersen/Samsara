3339f81fcda437ae4274ca4a0624728fbb169219
Marius Gundersen <me@mariusgundersen.net>
2015-02-01 11:45:58 +0100

    It will most likely be used behind a reverse proxy

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.enable('trust proxy');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
