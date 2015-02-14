dfaa50ee68e5344e9235faf096e8c07fbee0fdbe
Marius Gundersen <gundersen@gmail.com>
2015-02-14 14:22:12 +0100

    The config should be read-only, so it is frozen using deep-freeze

--- providers/app.js
+++ providers/app.js
@@ -1,11 +1,13 @@
 var fs = require('fs-promise');
 var Promise = require('promise');
+var freeze = require('deep-freeze');
 
 module.exports = function app(name){
   return {
     config: function(){
       return fs.readFile('config/apps/'+name+'/config.json')
-      .then(JSON.parse);
+      .then(JSON.parse)
+      .then(freeze);
     }
   };
 };
