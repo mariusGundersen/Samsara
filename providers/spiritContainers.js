f6f0191efebed3fc74041cd9cd3f40bfb1fb541f
Marius Gundersen <me@mariusgundersen.net>
2015-03-25 21:00:05 +0100

    Fixed sorting by version

--- providers/spiritContainers.js
+++ providers/spiritContainers.js
@@ -8,8 +8,6 @@ module.exports = function(name){
     return containers.filter(function(container){
       var match =  /^(.*?)(_v(\d+))?$/.exec(container.name);
       return match && match[1] == name;
-    }).sort(function(a,b){
-      return a.version < b.version ? 1 : a.version > b.version ? -1 : 0;
     }).map(function(container){
       return {
         spirit: name,
@@ -20,6 +18,8 @@ module.exports = function(name){
         status: container.status.split(' ').slice(container.state == 'running' ? 1 : 2).join(' '),
         version: (/^(.*?)(_v(\d+))?$/.exec(container.name)[3] || 0)|0
       };
+    }).sort(function(a,b){
+      return a.version < b.version ? 1 : a.version > b.version ? -1 : 0;
     });
   });
 };
