2c26a016bd3ec431bbc2fc1a667ee4e403817139
Marius Gundersen <me@mariusgundersen.net>
2015-04-12 19:51:16 +0200

    Used cachebust and max-age to cache static files

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,6 @@
 require.config({
   baseUrl: '/javascripts',
+  urlArgs: 'cachebust=' + document.querySelector('meta[name=cachebust]').getAttribute('content'),
   
   paths: {
     'knockout': '/bower_components/knockout/dist/knockout'
@@ -8,7 +9,6 @@ require.config({
   packages: [
     {name: 'deco', location: '/bower_components/deco/Dist', main: 'deco'}
   ]
-
 });
 
 require(['deco'], function(deco){
