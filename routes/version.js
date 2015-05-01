4393f2523015fd38134b131f4cb5306f2b375eb4
Marius Gundersen <me@mariusgundersen.net>
2015-05-01 15:18:07 +0200

    Limited log output to the last 50 lines and added download button for getting the entire log

--- routes/version.js
+++ routes/version.js
@@ -14,18 +14,11 @@ router.get('/:name/version/latest', co.wrap(function*(req, res, next){
 }));
 
 router.get('/:name/version/:version', co.wrap(function*(req, res, next){
-  const containers = yield spiritContainers(req.params.name);
-  const found = containers.filter(function(c){
-    return c.version == req.params.version;
-  })[0];
-
-  if(!found) throw new Error('404');
-
-  const container = docker.getContainer(found.id);
+  const container = yield getContainer(req.params.name, req.params.version);
 
   const config = yield container.inspect();
 
-  const logs = yield container.logs({stdout:true, stderr:true});
+  const logs = yield container.logs({stdout:true, stderr:true, tail: 50});
   const prettyLogs = yield prettifyLogs(logs);
 
   const pageModel = yield makePageModel(req.params.name + ' - ' + req.params.version, {
@@ -33,7 +26,7 @@ router.get('/:name/version/:version', co.wrap(function*(req, res, next){
     version: req.params.version,
     json: JSON.stringify(config, null, '  '),
     log: prettyLogs,
-    stopped: found.state === 'stopped',
+    stopped: !config.State.Running,
     model: {
       name: req.params.name,
       version: req.params.version
@@ -41,5 +34,25 @@ router.get('/:name/version/:version', co.wrap(function*(req, res, next){
   }, req.params.name, req.params.version);
   res.render('spirits/spirit/version/index', pageModel);
 }));
+
+router.get('/:name/version/:version/logs/download', co.wrap(function*(req, res, next){
+  const container = yield getContainer(req.params.name, req.params.version);
+  const config = yield container.inspect();
+  
+  res.setHeader('Content-disposition', 'attachment;filename='+config.Name.substr(1)+'.log');
+  const logs = yield container.logs({stdout:true, stderr:true});
+  logs.pipe(res);
+}));
+
+function* getContainer(name, version){
+  const containers = yield spiritContainers(name);
+  const found = containers.filter(function(c){
+    return c.version == version;
+  })[0];
+
+  if(!found) throw new Error('404');
+
+  return docker.getContainer(found.id);
+}
 
 module.exports = router;
