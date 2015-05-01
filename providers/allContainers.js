0149ab3fb5dbfd7550c0cc02b9752b852d4b35b0
Marius Gundersen <me@mariusgundersen.net>
2015-05-01 13:52:06 +0200

    Fixed an issue where containers that are linked to got the wrong name
    This finally explains why a container has a list of names

--- providers/allContainers.js
+++ providers/allContainers.js
@@ -3,13 +3,16 @@ const co = require('co');
 
 module.exports = co.wrap(function*(){
   const containers = yield docker.listContainers({all:true});
-  return Promise.all(containers.map(function(container){
+  return containers.map(function(container){
+    const name = container.Names.filter(function(name){
+      return name.lastIndexOf('/') === 0;
+    })[0].substr(1);
     return {
-      name: container.Names[0].substr(1),
+      name: name,
       id: container.Id,
       image: container.Image,
       state: container.Status.indexOf('Up')>=0 ? 'running' : 'stopped',
       status: container.Status
     };
-  }));
+  });
 });
