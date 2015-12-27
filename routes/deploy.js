9cffc5ee769a9167ab85958882ca0d975fd75d73
Marius Gundersen <me@mariusgundersen.net>
2015-12-27 16:01:27 +0100

    Removed the spirit config mutator since it is now part of samsara-lib

--- routes/deploy.js
+++ routes/deploy.js
@@ -1,38 +1,36 @@
 const router = require('express-promise-router')();
 const co = require('co');
-const mutateSpiritConfig = require('../mutators/spiritConfig')
+const samsara = require('samsara-lib')
 const deploy = require('../private/deploy').deploy;
 const validateDeploy = require('../private/validateDeploy');
 const request = require('request-promise');
 
 router.post('/:name/:secret', co.wrap(function*(req, res, next){
   console.log('deploying', req.params.name, req.body);
-  
-  try{    
-    yield validateDeploy(
-      req.params.name,
-      req.params.secret,
-      req.body.repository && req.body.repository.repo_name,
-      req.body.push_data && req.body.push_data.tag,
-      req.body.callback_url);
 
-    console.log('config is valid');
-    yield mutateSpiritConfig(req.params.name, function(config){
-      config.tag = req.body.push_data.tag;
-    });
-    
-    deployInBackground(name, req.body.callback_url)
-    .catch(function(error){
-      console.error(error);
-    });
-    
-    res.write('success');
+  try{
+	yield validateDeploy(
+	  req.params.name,
+	  req.params.secret,
+	  req.body.repository && req.body.repository.repo_name,
+	  req.body.push_data && req.body.push_data.tag,
+	  req.body.callback_url);
+
+	console.log('config is valid');
+	yield samsara().spirit(req.params.name).mutateConfig(config => config.tag = req.body.push_data.tag);
+
+	deployInBackground(name, req.body.callback_url)
+	.catch(function(error){
+	  console.error(error);
+	});
+
+	res.write('success');
   }catch(error){
-    console.log('validation failed for', req.params.name, error);
-    res.status('403');
-    res.write(JSON.stringify(error, null, ' '));
+	console.log('validation failed for', req.params.name, error);
+	res.status('403');
+	res.write(JSON.stringify(error, null, ' '));
   }finally{
-    res.end();
+	res.end();
   }
 }));
 
@@ -48,26 +46,26 @@ const deployInBackground = co.wrap(function*(name, callback_url){
   console.log('responding', result);
 
   yield request.post({
-    url: callback_url,
-    body: JSON.stringify(result)
+	url: callback_url,
+	body: JSON.stringify(result)
   });
 });
 
 function *tryDeploy(name){
   try{
-    console.log('deploying');
-    
-    yield deploy(name);
-    
-    return {
-      state: 'success',
-      description: 'deployed'
-    };
+	console.log('deploying');
+
+	yield deploy(name);
+
+	return {
+	  state: 'success',
+	  description: 'deployed'
+	};
   }catch(error){
-    return {
-      state: 'error',
-      description: error
-    };
+	return {
+	  state: 'error',
+	  description: error
+	};
   }
 }
 
