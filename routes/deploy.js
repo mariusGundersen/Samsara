56204f702724f37cfaff6b279acc5dd6bd1f632c
Marius Gundersen <me@mariusgundersen.net>
2015-12-27 20:21:39 +0100

    Simplified routes/deploy

--- routes/deploy.js
+++ routes/deploy.js
@@ -5,68 +5,43 @@ const deploy = require('../private/deploy').deploy;
 const validateDeploy = require('../private/validateDeploy');
 const request = require('request-promise');
 
-router.post('/:name/:secret', co.wrap(function*(req, res, next){
+router.post('/:name/:secret', co.wrap(function* (req, res, next) {
   console.log('deploying', req.params.name, req.body);
 
-  try{
-	yield validateDeploy(
-	  req.params.name,
-	  req.params.secret,
-	  req.body.repository && req.body.repository.repo_name,
-	  req.body.push_data && req.body.push_data.tag,
-	  req.body.callback_url);
+  try {
+    yield validateDeploy(
+      req.params.name,
+      req.params.secret,
+      req.body.repository && req.body.repository.repo_name,
+      req.body.push_data && req.body.push_data.tag,
+      req.body.callback_url);
 
-	console.log('config is valid');
-	yield samsara().spirit(req.params.name).mutateConfig(config => config.tag = req.body.push_data.tag);
+    console.log('config is valid');
+    yield samsara().spirit(req.params.name).mutateConfig(config => config.tag = req.body.push_data.tag);
 
-	deployInBackground(name, req.body.callback_url)
-	.catch(function(error){
-	  console.error(error);
-	});
+    deploy(name)
+      .then(success => ({state: 'success', description: 'deployed'}))
+      .catch(error => ({state: 'error', description: error}))
+      .then(result => request.post({
+        url: req.body.callback_url,
+        body: JSON.stringify(result)
+      }))
+      .catch(error => console.error(error));
 
-	res.write('success');
-  }catch(error){
-	console.log('validation failed for', req.params.name, error);
-	res.status('403');
-	res.write(JSON.stringify(error, null, ' '));
-  }finally{
-	res.end();
+    res.write('success');
+  } catch (error) {
+    console.log('validation failed for', req.params.name, error);
+    res.status('403');
+    res.write(JSON.stringify(error, null, ' '));
+  } finally {
+    res.end();
   }
 }));
 
-router.use(function(error, req, res, next){
+router.use(function (error, req, res, next) {
   res.status('500');
   res.write(JSON.stringify(error.message || error, null, ' '));
   res.end();
 });
-
-const deployInBackground = co.wrap(function*(name, callback_url){
-  const result = yield tryDeploy(name);
-
-  console.log('responding', result);
-
-  yield request.post({
-	url: callback_url,
-	body: JSON.stringify(result)
-  });
-});
-
-function *tryDeploy(name){
-  try{
-	console.log('deploying');
-
-	yield deploy(name);
-
-	return {
-	  state: 'success',
-	  description: 'deployed'
-	};
-  }catch(error){
-	return {
-	  state: 'error',
-	  description: error
-	};
-  }
-}
 
 module.exports = router;
