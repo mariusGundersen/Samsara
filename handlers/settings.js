d13adb9f6de0c8a2dbf24af2d2d2012bbcc62338
Marius Gundersen <me@mariusgundersen.net>
2015-12-27 22:55:25 +0100

    The provider/mutator directories were a bad idea, so I got rid of them

--- handlers/settings.js
+++ handlers/settings.js
@@ -1,22 +1,23 @@
+'use strict'
 const qvc = require('qvc');
 const NotEmpty = require('qvc/constraints/NotEmpty');
-const authentication = require('../mutators/authentication');
+const authentication = require('../private/authentication');
 const md5 = require('apache-md5');
 
 module.exports = [
   qvc.command('setAuthentication', function(command){
-    return authentication(function(entries){
-      const found = entries.filter(function(entry){
+    return authentication.mutate(function(entries){
+      let found = entries.filter(function(entry){
         return entry.username == command.username;
       })[0];
-      
+
       if(found == null){
         found = {
           username: command.username
         };
         entries.push(found);
       }
-      
+
       found.secret = md5(command.password);
     });
   }, {
--- private/authentication.js
+++ private/authentication.js
@@ -1,7 +1,8 @@
+'use strict'
 const fs = require('fs-promise');
 const co = require('co');
 
-module.exports = co.wrap(function*(){
+const users = co.wrap(function*(){
   const contents = yield getAuth();
   return contents.split('\n')
   .filter(entry => entry.length)
@@ -12,6 +13,21 @@ module.exports = co.wrap(function*(){
   }));
 });
 
+const mutate = co.wrap(function*(mutate){
+  const entries = yield users();
+  const mutatedEntries = yield (mutate(entries) || entries);
+  const contents = mutatedEntries.map(function(entry){
+    return entry.username+':'+entry.secret;
+  }).join('\n')+'\n';
+
+  return fs.writeFile('config/authentication', contents);
+});
+
+module.exports = {
+  mutate: mutate,
+  users: users
+};
+
 function* getAuth(){
   const authfile = 'config/authentication';
   try{
@@ -19,6 +35,6 @@ function* getAuth(){
   }catch(e){
     console.error(e);
   }
-  
+
   return 'admin:$apr1$TtDE7V/O$3MhOE98M30cWTDdrb1z7q1';
 }
