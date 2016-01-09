0c2cbdddab9560930b5ac4ae08a963cbc202dbd2
Marius Gundersen <me@mariusgundersen.net>
2016-01-09 20:29:19 +0100

    Moved prettifying of logs to samsara-lib

--- routes/container.js
+++ routes/container.js
@@ -2,7 +2,6 @@ const router = require('express-promise-router')();
 const co = require('co');
 const samsara = require('samsara-lib');
 const makePageModel = require('../pageModels/containers');
-const prettifyLogs = require('../private/prettifyLogs');
 
 router.get('/', co.wrap(function*(req, res, next) {
   const list = yield samsara().containers();
@@ -13,13 +12,13 @@ router.get('/', co.wrap(function*(req, res, next) {
 router.get('/:id', co.wrap(function*(req, res, next) {
   const container = samsara().container(req.params.id);
   const config = yield container.inspect();
-  const logs = yield container.logs({stdout:true, stderr:true, tail:50});
+  const logs = yield container.prettyLogs(true, {stdout:true, stderr:true, tail:50});
 
   const pageModel = yield makePageModel(config.Name.substr(1) + ' - Container', {
     info: config,
     name: config.Name.substr(1),
     json: JSON.stringify(config, null, '  '),
-    log: logs.pipe(prettifyLogs()),
+    log: logs,
     controls: {
       id: config.Id,
       name: config.Name.substr(1),
@@ -35,7 +34,7 @@ router.get('/:id/logs/download', co.wrap(function*(req, res, next){
   const config = yield container.inspect();
 
   res.setHeader('Content-disposition', 'attachment;filename='+config.Name.substr(1)+'.log');
-  const logs = yield container.logs({stdout:true, stderr:true});
+  const logs = yield container.prettyLogs(false, {stdout:true, stderr:true});
   logs.pipe(res);
 }));
 
--- routes/life.js
+++ routes/life.js
@@ -1,7 +1,6 @@
 const router = require('express-promise-router')();
 const samsara = require('samsara-lib');
 const makePageModel = require('../pageModels/life');
-const prettifyLogs = require('../private/prettifyLogs');
 const co = require('co');
 
 router.get('/:name/life/latest', co.wrap(function*(req, res, next){
@@ -17,7 +16,7 @@ router.get('/:name/life/:life', co.wrap(function*(req, res, next){
   const life = spirit.life(req.params.life);
 
   const status = yield life.status;
-  const container = yield tryGetContainer(life);
+  const container = yield life.container;
 
   const pageModel = yield makePageModel(req.params.name + ' - ' + req.params.life, {
     name: req.params.name,
@@ -29,15 +28,15 @@ router.get('/:name/life/:life', co.wrap(function*(req, res, next){
       },
       name: req.params.name,
       life: req.params.life,
-      json: container.inspect,
+      json: life.inspect.then(json => json ? JSON.stringify(json, null, '  ') : ''),
       config: life.containerConfig.catch(e => ''),
-      log: container.logs,
+      log: life.containerLog(true, {stdout:true, stderr:true, tail: 50}),
       deploy: life.deployLog.catch(e => ''),
     },
     revive: {
       name: req.params.name,
       life: req.params.life,
-      revivable: status == 'stopped' && container.exists
+      revivable: status == 'stopped' && !!container
     },
     deploy: {
       name: req.params.name,
@@ -51,32 +50,13 @@ router.get('/:name/life/:life', co.wrap(function*(req, res, next){
 router.get('/:name/life/:life/logs/download', co.wrap(function*(req, res, next){
   const name = req.params.name;
   const life = req.params.life;
-  const container = yield samsara().spirit(name).life(life).container;
+  const logs = yield samsara()
+    .spirit(name)
+    .life(life)
+    .containerLog(false, {stdout: true, stderr: true});
 
   res.setHeader('Content-disposition', `attachment;filename=${name}_v${life}.log`);
-  const logs = yield container.logs({stdout:true, stderr:true});
-  logs.pipe(prettifyLogs({html:false})).pipe(res);
+  logs.pipe(res);
 }));
-
-function *tryGetContainer(life){
-  try{
-    const container = yield life.container;
-
-    const inspect = yield container.inspect();
-    const logs = yield container.logs({stdout:true, stderr:true, tail: 50});
-
-    return {
-      inspect: JSON.stringify(inspect, null, '  '),
-      logs: logs.pipe(prettifyLogs()),
-      exists: true
-    };
-  }catch(e){
-    return {
-      inspect: '',
-      logs: '',
-      exists: false
-    };
-  }
-}
 
 module.exports = router;
