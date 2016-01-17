fee09a77a0cae84dd1ee540ddda67a385ad9b833
Marius Gundersen <me@mariusgundersen.net>
2016-01-17 19:15:59 +0100

    Theres no menu in the error

--- app.js
+++ app.js
@@ -60,7 +60,6 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       title: 'Error',
-      mentu:{settings:false},
       content: {
         message: err.message,
         error: app.get('env') === 'development' ? err : {}
