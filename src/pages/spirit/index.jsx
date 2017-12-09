2f6c9c5782017bf9f652b137b1cbfac38c9dc227
Marius Gundersen <me@mariusgundersen.net>
2017-12-09 15:52:08 +0100

    Fixed link to life

--- src/pages/spirit/index.jsx
+++ src/pages/spirit/index.jsx
@@ -6,7 +6,7 @@ import nth from '../../private/nthLife';
 
 export default ({name, life, description, controls, deploy}) => (
   <div className="pane content">
-    <h2><Icon of="info-circle" />{name} (<a className="no-link" href={`${name}/life/${life}`}>{nth(life)} life</a>)</h2>
+    <h2><Icon of="info-circle" />{name} (<a className="no-link" href={`/spirit/${name}/life/${life}`}>{nth(life)} life</a>)</h2>
         <p className="text-center">
           {description}
         </p>
