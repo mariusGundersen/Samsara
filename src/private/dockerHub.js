c6cc74f50a8b262b882b54aaa57dc61294ee78ea
Marius Gundersen <me@mariusgundersen.net>
2022-11-10 21:58:40 +0100

    fixed tag issue

--- src/private/dockerHub.js
+++ src/private/dockerHub.js
@@ -1,20 +1,29 @@
-import request from 'request-promise';
+import request from "request-promise";
 
-export async function searchImages(term){
-  return await requestJson(`https://registry.hub.docker.com/v1/search?q=${encodeURIComponent(term)}&n=5`);
-};
+export async function searchImages(term) {
+  return await requestJson(
+    `https://registry.hub.docker.com/v1/search?q=${encodeURIComponent(
+      term
+    )}&n=5`
+  );
+}
 
-export async function searchImageTags(term){
-  let data = await requestJson(`https://hub.docker.com/v2/repositories/${term}/tags`);
+export async function searchImageTags(term = "") {
+  if (!term.includes("/")) {
+    term = "library/" + term;
+  }
+  let data = await requestJson(
+    `https://hub.docker.com/v2/repositories/${term}/tags`
+  );
   let list = data.results;
-  while(data.next){
+  while (data.next) {
     data = await requestJson(data.next);
     list = list.concat(data.results);
   }
   return list;
-};
+}
 
-async function requestJson(url){
+async function requestJson(url) {
   const result = await request(url);
   return JSON.parse(result);
 }
